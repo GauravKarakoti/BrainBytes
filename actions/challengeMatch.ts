7272c5eadc3ba22364207358cf7f3fd23319a36f
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-11-09 00:51:51 +0530

    Cluster

--- actions/challengeMatch.ts
+++ actions/challengeMatch.ts
@@ -11,7 +11,7 @@ const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
 })
 
